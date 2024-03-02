52366981799936eecc06ab92c9f28a099048e77a
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-03-02 18:46:45 +0900

    add Project

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+  name: "swift-bug-same-argument",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v11),
+  ],
+  products: [
+    .library(
+      name: "Library1",
+      targets: ["Library1"]
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-http-types", from: "1.0.3"),
+  ],
+  targets: [
+    .target(
+      name: "Library1",
+      dependencies: [
+        .product(name: "HTTPTypes", package: "swift-http-types"),
+        .product(name: "HTTPTypesFoundation", package: "swift-http-types"),
+      ]
+    ),
+  ]
+)
---
+++ Sources/Library1/Library1.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+import HTTPTypes
+import HTTPTypesFoundation
+
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
+#endif
+
+
+public protocol Request {
+  var method: HTTPRequest.Method { get }
+  var url: URL { get }
+  var headers: HTTPFields { get }
+}
+
+extension URLSession {
+  func data(for request: some Request) async throws -> (Data, HTTPResponse) {
+    let request = HTTPRequest(
+      method: request.method,
+      url: request.url,
+      headerFields: request.headers
+    )
+    return try await self.data(for: request)
+  }
+}
